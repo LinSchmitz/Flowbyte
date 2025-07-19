8d846490c609a4282463c47caee7dab0a138e45f
naleilan <leilan.n@gmail.com>
2025-07-19 07:47:18 +0330

    added Analyz

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ export default function Header() {
   return (
     <header className="header">
       <div className="logo">📊 Flowbyte </div>
+      <h4>Success doesn't come from intensity — it comes from consistency.</h4>
       <div className="profile-icon">👤</div>
     </header>
   );
